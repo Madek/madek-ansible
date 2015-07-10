63b906da8c01324ff8fac6ec9e57f83eb70e6812
Thomas Schank <DrTom@schank.ch>
2015-07-10 08:50:51 +0200

    Severing pages via context but assets paths are broken

--- roles/madek-webapp/templates/start.sh
+++ roles/madek-webapp/templates/start.sh
@@ -5,6 +5,6 @@ source /etc/profile.d/rbenv-load.sh \
 && rbenv shell {{rubies.jruby.version}} \
 && export JVM_OPTS="-Xmx{{madek_webapp_xmx_value}}" \
 && export RAILS_ENV={{rails_env}}  \
-&& export RAILS_RELATIVE_URL_ROOT="/" \
-&& bundle exec puma config.ru -t 4:40 -b tcp://127.0.0.1:8880 \
+&& export RAILS_RELATIVE_URL_ROOT="{{madek_webapp_context}}" \
+&& bundle exec puma config.ru -t 4:40 -b tcp://127.0.0.1:{{madek_webapp_port}} \
   >> {{madek_webapp_log_dir}}/{{madek_webapp_service_name}}.log 2>&1
