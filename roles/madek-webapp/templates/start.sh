b496e09024872bec79c52544a1025dfe908c38a9
Thomas Schank <DrTom@schank.ch>
2015-07-29 09:46:25 +0200

    Fix a ton of stuff

--- roles/madek-webapp/templates/start.sh
+++ roles/madek-webapp/templates/start.sh
@@ -6,5 +6,6 @@ source /etc/profile.d/rbenv-load.sh \
 && export JVM_OPTS="-Xmx{{madek_webapp_xmx_value}}" \
 && export RAILS_ENV={{rails_env}}  \
 && export RAILS_RELATIVE_URL_ROOT="{{madek_webapp_context}}" \
+&& export MADEK_ROOT_DIR="{{madek_root_dir}}" \
 && bundle exec puma config.ru -t 4:40 -b tcp://127.0.0.1:{{madek_webapp_port}} \
   >> {{madek_webapp_log_dir}}/{{madek_webapp_service_name}}.log 2>&1
